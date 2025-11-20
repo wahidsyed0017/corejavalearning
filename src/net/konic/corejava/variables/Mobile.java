7512e622d0b3ebc53fc7696eca88cd39c858ceaa
wahidsyed0017 <wahidsyed0017@gmail.com>
2025-11-20 12:51:06 +0530

    Initial Commit

---
+++ src/net/konic/corejava/variables/Mobile.java
@@ -0,0 +1,18 @@
+package net.konic.corejava.variables;
+
+public class Mobile {
+
+	public static void main(String[] args) {
+		
+		
+	String Brandname = "apple";
+	String feauters =  "a19 chip";
+	float inches = 6.1f;
+	
+	
+	System.out.println("apple");
+	System.out.println("feauters");
+	System.out.println(inches);
+	
+	}
+}
---
+++ src/net/konic/corejava/variables/car.java
@@ -0,0 +1,30 @@
+package net.konic.corejava.variables;
+
+public class car {
+	
+	public static void main(String[] args) {
+		
+		String company = "TATA";
+		int CC = 1390;
+		String engine = "zest";
+		String type = "seadan";
+		String carname = "tigor";
+		String seating = "5seating";
+		String fueltype = "diesel";
+		int numberofwheels = 4*4;
+		int fuelcapacity = 22;
+		
+		
+		
+		
+		System.out.println("company :" +company);
+		System.out.println("engine:"   +  engine);
+		System.out.println("CC:"  +   CC);
+		System.out.println("carname:"   +  carname);
+		System.out.println("seating:"  + seating);
+		System.out.println("fueltype:"  + fueltype);
+		System.out.println("numberofwheels");
+		System.out.println("fuelcapacity in liters :" + fuelcapacity);
+	}
+
+}
